fb861d240fe4466c8555b7f81e4c290ac4a6acd8
wz1310 <a002041zano@gmail.com>
2026-04-07 20:09:28 +0700

    update

--- approval_matrix/__manifest__.py
+++ approval_matrix/__manifest__.py
@@ -9,9 +9,6 @@
         Approval Matrix for any model. You just only need call approval_matrix_validation()
     """,
 
-    'author': "Portcities",
-    'website': "http://www.portcities.net",
-
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
@@ -19,7 +16,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','web_many2one_reference','hr','sale'],
+    'depends': ['base','web_many2one_reference','sale'],
 
     # always loaded
     'data': [
