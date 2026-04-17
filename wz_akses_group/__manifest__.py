14aeadb42b0e123572c91a0e3ccce4289e3ad9cf
wz1310 <a002041zano@gmail.com>
2026-04-17 18:36:54 +0700

    update

---
+++ wz_akses_group/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Inherit Group ',
+    'summary': """Inherit Group """,
+    'version': '0.0.1',
+    'author': 'Wiza',
+    'description': """Inherit Group """,
+    'depends': ['contacts','mrp','approval_matrix', 'web_many2one_reference'],
+    'data': [
+        'views/group_view.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True
+}
