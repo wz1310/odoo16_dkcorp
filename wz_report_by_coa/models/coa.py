59eacb73f190740936b24e8819308279d467adec
wz1310 <a002041zano@gmail.com>
2026-09-08 18:21:07 +0700

    update

--- wz_report_by_coa/models/coa.py
+++ wz_report_by_coa/models/coa.py
@@ -68,7 +68,7 @@ class StockMove(models.Model):
 			# karena Odoo akan memanggilnya secara otomatis di alur _action_done()
 			
 		return svl_moves
-		
+
 	def _account_entry_move(self, qty, description, svl_id, cost):
 		if self.product_id.type == 'service' and self.raw_material_production_id:
 			am_vals = []
@@ -78,8 +78,8 @@ class StockMove(models.Model):
 			company_from = self._is_out() and self.mapped('move_line_ids.location_id.company_id') or False
 			journal_id, acc_src, acc_dest, acc_valuation = self._get_accounting_data_for_valuation()
 
-			# Ambil akun expense (kredit) menggunakan fungsi _get_src_account yang sudah dibuat
-			credit_account_id = self._get_src_account({
+			# Ambil akun Expense (untuk posisi Kredit)
+			expense_account_id = self._get_src_account({
 				'stock_input': self.env['account.account'].browse(acc_src),
 				'stock_output': self.env['account.account'].browse(acc_dest),
 				'stock_valuation': self.env['account.account'].browse(acc_valuation),
@@ -87,13 +87,15 @@ class StockMove(models.Model):
 
 			if self._is_out():
 				cost = -1 * cost
-				# Gunakan credit_account_id (Expense) pada posisi Kredit, menggantikan acc_dest
+				# POSISI DIPERBAIKI:
+				# Parameter 1 (DEBIT)  : acc_valuation atau acc_dest (Interim Stock/WIP)
+				# Parameter 2 (KREDIT) : expense_account_id (Biaya Gaji)
 				am_vals.append(self.with_company(company_from)._prepare_account_move_vals(
-					acc_valuation, credit_account_id, journal_id, qty, description, svl_id, cost
+					acc_dest, expense_account_id, journal_id, qty, description, svl_id, cost
 				))
 			return am_vals
 
-		return super(StockMove, self)._account_entry_move(qty, description, svl_id, cost)
+	return super(StockMove, self)._account_entry_move(qty, description, svl_id, cost)
 
 
 class MrpProduction(models.Model):
