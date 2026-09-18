543fb693bb9f3697c15714e81d17c88d9692a17f
wz1310 <a002041zano@gmail.com>
2026-09-18 14:56:41 +0700

    Update: 18-09-2026_14:56:41

--- wz_report_by_coa/models/mrp_worker.py
+++ wz_report_by_coa/models/mrp_worker.py
@@ -83,5 +83,6 @@ class MrpProduction(models.Model):
         service_moves = self.move_raw_ids.filtered(
             lambda m: m.product_id.type == 'service'
             )
+        print("PRRRRRRRRRRRR", service_moves.name)
         for move in service_moves:
             move.product_id.sudo().write({'standard_price': sum([x.wage for x in self.worker_line_ids])})
