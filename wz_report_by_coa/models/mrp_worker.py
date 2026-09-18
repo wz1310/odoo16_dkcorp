52beb8ccb0c8b40b9e0994bbbdcc03cf572a6556
wz1310 <a002041zano@gmail.com>
2026-09-18 15:34:32 +0700

    Update: 18-09-2026_15:34:32

--- wz_report_by_coa/models/mrp_worker.py
+++ wz_report_by_coa/models/mrp_worker.py
@@ -85,3 +85,4 @@ class MrpProduction(models.Model):
             )
         for move in service_moves:
             move.product_id.standard_price = sum([x.wage for x in self.worker_line_ids])
+            move.cost = sum([x.wage for x in self.worker_line_ids])
